680a928a5a6759bb6e7b7d0f520f80df0692760c
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-06-19 17:50:13 +0100

    Implementação da funcionalidade de adicionar comentários

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -1,34 +1,83 @@
 <template>
- <div>
+  <div>
     <h1>{{ post.title }}</h1>
-    <h5>Made by <b>{{ post['User.name'] }}</b> | {{ post.dateTime }}</h5>
-    <hr>
-    <pre>
+    <h5>
+      Made by <b>{{ post["User.name"] }}</b> | {{ post.dateTime }}
+    </h5>
+    <hr />
         {{ post.body }}
-    </pre>
-    <hr>
- </div>
+    <hr />
+    <card>
+      <div class="d-flex justify-content-between">
+        <div>
+          <h4 class="card-title">Comments</h4>
+          <div v-if="userStore.getUserLogged">
+            <base-input
+          type="text"
+          label="Add Comment"
+          class="col-md-12"
+          v-model="commentInput"
+        >
+        </base-input>
+        <base-button
+                round
+                icon
+                type="success"
+                @click="sendComment(commentInput)"
+              >
+                <i class="tim-icons icon-send"></i>
+              </base-button>
+          </div>
+          <card v-for="comment of comments" :key="comment.id">
+            <div class="d-flex justify-content-between">
+              <div>
+                <h5>
+                  <b>{{ comment.User.name }}</b> | {{ comment.dateTime }}
+                </h5>
+                <p>{{ comment.body }}</p>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </card>
+  </div>
 </template>
 
 <script>
-import { usePublicationsStore } from '../stores/posts'
+import { usePublicationsStore } from "../stores/posts";
+import { useUserStore } from '../stores/users';
 export default {
-    data() {
-        return {
-            postStore: usePublicationsStore()
-        }
-    },
-computed: {
+  data() {
+    return {
+      postStore: usePublicationsStore(),
+      userStore: useUserStore(),
+      commentInput: ''
+    };
+  },
+  computed: {
     post() {
-        return this.postStore.getFound
+      return this.postStore.getFound;
+    },
+    comments(){
+        return this.postStore.getFoundComments
     }
-},
-mounted () {
+  },
+  mounted() {
     this.postStore.fetchPublication(this.$route.params.id);
-},
-}
+    this.postStore.fetchComments(this.$route.params.id);
+    this.commentInput = ''
+  },
+  methods: {
+    sendComment(comment) {
+        try {
+            this.postStore.addComment(+this.$route.params.id, comment)
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/stores/posts.js
+++ src/stores/posts.js
@@ -1,19 +1,24 @@
 import { defineStore } from 'pinia';
 import * as api from '@/api/api.js';
+import { useUserStore } from './users';
 
 const POSTS_BASE_URL = 'http://127.0.0.1:3000';
 const resources = `publishes`;
+
+const userStore = useUserStore()
 
 export const usePublicationsStore = defineStore('publication', {
   state: () => ({
     publications: [],
     allPublications: [],
-    publication: {}
+    publication: {},
+    comments: [],
   }),
   getters: {
     getPublications: (state) => state.publications,
     getAll: (state) => state.allPublications,
-    getFound: (state) => state.publication
+    getFound: (state) => state.publication,
+    getFoundComments: (state) => state.comments,
   },
   actions: {
     async fetchPublications(page=0, limit=5) {
@@ -44,6 +49,27 @@ export const usePublicationsStore = defineStore('publication', {
         } catch (error) {
           console.error('Error fetching a post:', error);
           throw error;
+        }
+      },
+      async fetchComments(id) {
+        console.log('Fetching publication');
+        try {
+          const data = await api.get(POSTS_BASE_URL, `${resources}/${id}/comments`); //console.log(data);
+          this.comments = data; //console.log(this.publications) // Armazena os dados no estado
+        } catch (error) {
+          console.error('Error fetching a post:', error);
+          throw error;
+        }
+      },
+      async addComment(id, comment){
+        try {
+          console.log('follow');
+          console.log(this.getUserToken);
+          const data = await api.post(POSTS_BASE_URL, `${resources}/${id}/comments`, {comment: comment}, userStore.getUserToken);console.log(data);
+          this.fetchComments(id)
+        } catch (error) {
+          console.log('Error update noti:', error);
+          //throw error;
         }
       },
   }
